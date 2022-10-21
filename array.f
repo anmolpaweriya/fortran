b46fa3710f59942830bcd598972ab1ec20a3afd1
Anisole <64343922+anisolepro@users.noreply.github.com>
2022-10-21 11:59:58 +0500

    Update array.f

--- array.f
+++ array.f
@@ -1,12 +1,12 @@
 ! array in fortran
       program array
       implicit none
-      real ::a(6)
+      real ::a(0:6)      ! default index start from 1 to specifically start it from 0 we use (0:n) this syntax
       integer :: n
       do n = 1,6
       print 6,n,"value"
 6     format(i1,a6)
-      read(*,*)a(n)      ! index start from 1
+      read(*,*)a(n)
       print*, ""
       end do
 
